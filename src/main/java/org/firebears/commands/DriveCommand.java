fd97ba39504a91ef9ecfca5bd9fafa4b61c13c97
Nicholas S <nschense@gmail.com>
2019-02-09 15:46:40 -0600

    Added Deadband to DriveCommand

--- src/main/java/org/firebears/commands/DriveCommand.java
+++ src/main/java/org/firebears/commands/DriveCommand.java
@@ -12,11 +12,13 @@ public class DriveCommand extends Command {
     final Preferences config;
     int joystickSpeedAxis;
     int joystickRotateAxis;
+    double adjust;
 
     public DriveCommand() {
         config = Preferences.getInstance();
         joystickSpeedAxis = config.getInt("joystick1.speedAxis", 1);
         joystickRotateAxis = config.getInt("joystick1.rotateAxis", 4);
+        adjust = config.getDouble("driveCommand.deadBand", 0.1);
         requires(Robot.chassis);
     }
 
@@ -29,6 +31,21 @@ public class DriveCommand extends Command {
         double speed = -1 * Robot.oi.controller1.getRawAxis(joystickSpeedAxis);
         double rotation = Robot.oi.controller1.getRawAxis(joystickRotateAxis);
         Robot.chassis.drive(speed, rotation);
+    }
+
+    private double deadBand(double x){
+        
+        double slope = 1/(1-adjust);
+
+        if (Math.abs(x) <= adjust){
+            return 0.0;
+        } else if (Math.abs(x) == 1) {
+            return x;
+        } else {
+            return slope*x - (slope-1);
+        }
+
+        
     }
 
     @Override
