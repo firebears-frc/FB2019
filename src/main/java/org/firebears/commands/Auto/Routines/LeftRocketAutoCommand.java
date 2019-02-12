4204aac162b6e8c8302e88d1a402d71ddd91f1bc
Abby Kunkle <robotsandsugar@gmail.com>
2019-02-11 21:02:17 -0600

    Auto selector with three autos

---
+++ src/main/java/org/firebears/commands/Auto/Routines/LeftRocketAutoCommand.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.firebears.commands.Auto.Routines;
+
+import org.firebears.commands.DistanceCommand;
+import org.firebears.commands.RotateToAngleCommand;
+import org.firebears.commands.ResetNavXCommand;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class LeftRocketAutoCommand extends CommandGroup {
+  public LeftRocketAutoCommand() {
+    addSequential(new ResetNavXCommand());
+    //addSequential(new WaitCommand(.25));
+    addSequential(new DistanceCommand(60));
+    // addSequential(new WaitCommand(.25));
+    addSequential(new RotateToAngleCommand(-90));
+    addSequential(new DistanceCommand(52));
+   //  addSequential(new WaitCommand(.25));
+    addSequential(new RotateToAngleCommand(-30));
+    addSequential(new DistanceCommand(45));
+  }
+}
