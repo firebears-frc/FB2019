b3e59ae1d2b289873f97d7302c27b86f0f7865fa
Keith Rieck <KeithRieck@gmail.com>
2019-02-17 14:11:56 -0600

    Added lights orchestration

--- src/main/java/org/firebears/subsystems/Lights.java
+++ src/main/java/org/firebears/subsystems/Lights.java
@@ -1,7 +1,9 @@
 package org.firebears.subsystems;
 
+import org.firebears.Robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.Preferences;
@@ -22,32 +24,34 @@ public class Lights extends Subsystem {
 	public static final int RAINBOW_ANIMATION = 4;
 	public static final int PULSE_ANIMATION = 5;
 	public static final int ROCKET_ANIMATION = 6;
-	
+
 	public static final int ELEVATOR_STRIP = 0;
 	public static final int SUPPORT_STRIP = 1;
 	public static final int AFRAME_STRIP = 2;
-	
+
 	private final I2C i2c;
 	private final DriverStation driverstation;
 	private final boolean DEBUG;
+	private long celebrateTimeout = -1;
 	private byte[] dataOut = new byte[2];
 	private byte[] dataBack = new byte[0];
 	private int[] currentAnimation = new int[MAX_PIXELSTRIPS];
 	private int[] nextAnimation = new int[MAX_PIXELSTRIPS];
-	
-    public Lights() {
-        final Preferences config = Preferences.getInstance();
-        i2c =  new I2C(Port.kOnboard, config.getInt("lights.i2cAddress", 4));
-        driverstation = DriverStation.getInstance();
-        DEBUG = config.getBoolean("debug", false);
-    }
 
-    @Override
-    public void initDefaultCommand() { }
+	public Lights() {
+		final Preferences config = Preferences.getInstance();
+		i2c = new I2C(Port.kOnboard, config.getInt("lights.i2cAddress", 4));
+		driverstation = DriverStation.getInstance();
+		DEBUG = config.getBoolean("debug", false);
+	}
 
-    /**
-     * Find all changed animations and push the changes out to the Arduino.
-     */
+	@Override
+	public void initDefaultCommand() {
+	}
+
+	/**
+	 * Find all changed animations and push the changes out to the Arduino.
+	 */
 	private synchronized boolean sendAllAnimations() {
 		boolean messagesSent = false;
 		for (int s = 0; s < MAX_PIXELSTRIPS; s++) {
@@ -65,7 +69,7 @@ public class Lights extends Subsystem {
 		}
 		return messagesSent;
 	}
-	
+
 	/**
 	 * Reset all strips to their startup animations.
 	 */
@@ -74,29 +78,61 @@ public class Lights extends Subsystem {
 			currentAnimation[s] = -1;
 			nextAnimation[s] = currentAnimation[s];
 		}
-		setAnimation(ELEVATOR_STRIP, RED_ANIMATION);
-		setAnimation(SUPPORT_STRIP, RED_ANIMATION);
-		setAnimation(AFRAME_STRIP, RED_ANIMATION);
+		int defaultAnimation = driverstation.getAlliance().equals(Alliance.Blue) ? BLUE_ANIMATION : RED_ANIMATION;
+		setAnimation(ELEVATOR_STRIP, ROCKET_ANIMATION);
+		setAnimation(SUPPORT_STRIP, defaultAnimation);
+		setAnimation(AFRAME_STRIP, defaultAnimation);
+	}
+
+	public void setCelebrateMode(boolean celebrate) {
+		if (celebrate) {
+			celebrateTimeout = System.currentTimeMillis() + 3 * 3000L;
+		} else {
+			celebrateTimeout = -1;
+		}
+	}
+
+	public boolean isCelebrating() {
+		if (celebrateTimeout < 0 || System.currentTimeMillis() > celebrateTimeout) {
+			celebrateTimeout = -1;
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 	/**
 	 * Change one strip's animation.
+	 * 
 	 * @param s Strip number.
 	 * @param a Animation number.
 	 */
 	private synchronized void setAnimation(int s, int a) {
 		nextAnimation[s] = a;
 	}
-	
+
 	/**
 	 * Change the lights animations based on changes within the robot.
 	 */
-    @Override
-    public void periodic() {
-    	
-        // TODO: Put lights orchestration here
-    	
-    	sendAllAnimations();
-    }
-}
+	@Override
+	public void periodic() {
+		int defaultAnimation = driverstation.getAlliance().equals(Alliance.Blue) ? BLUE_ANIMATION : RED_ANIMATION;
 
+		if (Robot.chassis.isTipping()) {
+			setAnimation(ELEVATOR_STRIP, TIPPINGLIGHT_ANIMATION);
+			setAnimation(SUPPORT_STRIP, TIPPINGLIGHT_ANIMATION);
+			setAnimation(AFRAME_STRIP, TIPPINGLIGHT_ANIMATION);
+		} else if (isCelebrating()) {
+			setAnimation(ELEVATOR_STRIP, RAINBOW_ANIMATION);
+			setAnimation(SUPPORT_STRIP, RAINBOW_ANIMATION);
+			setAnimation(AFRAME_STRIP, RAINBOW_ANIMATION);
+		} else {
+			setAnimation(ELEVATOR_STRIP, defaultAnimation);
+			setAnimation(SUPPORT_STRIP, defaultAnimation);
+			setAnimation(AFRAME_STRIP, defaultAnimation);
+			// TODO : add lots more lights orchestration here
+		}
+
+		sendAllAnimations();
+	}
+}
