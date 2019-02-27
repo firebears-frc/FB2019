d9bfafb95586cbe3b63d987bfec8cf14ad459b5d
seth <falugger1@gmail.com>
2019-02-26 19:21:57 -0600

    Added widgets to lidars

--- src/main/java/org/firebears/subsystems/CargoGrabber.java
+++ src/main/java/org/firebears/subsystems/CargoGrabber.java
@@ -1,5 +1,8 @@
 package org.firebears.subsystems;
 
+import org.firebears.Robot;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -16,12 +19,19 @@ public class CargoGrabber extends Subsystem {
     public DigitalInput cargoLeftSensor;
     public DigitalInput cargoRightSensor;
 
+    private final NetworkTableEntry leftSensorWidget;
+    private final NetworkTableEntry rightSensorWidget;
+    private final NetworkTableEntry cargoCapturedSensorWidget;
+
     public CargoGrabber() {
         motor = new WPI_TalonSRX(config.getInt("cargoGrabber.motor.canID", 13));
         motor.setNeutralMode(NeutralMode.Brake);
         cargoCapturedSensor = new DigitalInput(config.getInt("cargoGrabcber.cargoCaptured.dio", 6));
         cargoLeftSensor = new DigitalInput(config.getInt("cargoGrabcber.cargoLeft.dio", 7));
         cargoRightSensor = new DigitalInput(config.getInt("cargoGrabcber.cargoRight.dio", 8));
+        leftSensorWidget = Robot.programmerTab.add("Left Sensor", false).getEntry();
+        rightSensorWidget = Robot.programmerTab.add("Left Sensor", false).getEntry();
+        cargoCapturedSensorWidget = Robot.programmerTab.add("Left Sensor", false).getEntry();
     }
 
     @Override
@@ -32,7 +42,9 @@ public class CargoGrabber extends Subsystem {
     @Override
     public void periodic() {
         SmartDashboard.putNumber("cargoMotorSpeed", motor.get());
-
+        leftSensorWidget.setBoolean(cargoLeftSensor.get());
+        rightSensorWidget.setBoolean(cargoRightSensor.get());
+        cargoCapturedSensorWidget.setBoolean(cargoCapturedSensor.get());
     }
 
     public void intake() {
