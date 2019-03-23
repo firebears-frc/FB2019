0b7db628a72e8bd2e2293dff77e98b0139d22330
Keith Rieck <KeithRieck@gmail.com>
2019-03-22 21:15:35 -0500

    Added frogger encoder

--- src/main/java/org/firebears/subsystems/Frogger.java
+++ src/main/java/org/firebears/subsystems/Frogger.java
@@ -3,13 +3,19 @@ package org.firebears.subsystems;
 import org.firebears.Robot;
 import org.firebears.commands.FroggerLowerCommand;
 
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.DigitalInput;
+
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Frogger extends Subsystem {
     final Preferences config = Preferences.getInstance();
@@ -17,9 +23,11 @@ public class Frogger extends Subsystem {
     WPI_TalonSRX forwardMotor;
     private final double FROGGER_SPEED = 0.75;
     private final double DRIVE_SPEED = 1.0;
-
+    private final double TICKS_PER_INCH = 113.0;
     private final NetworkTableEntry froggerBottomWidget;
-    
+    //private final NetworkTableEntry froggerTopWidget;
+    private final Encoder encoder;
+    double startingDistance;
 
     public Frogger() {
         jumpMotor = new WPI_TalonSRX(config.getInt("frogger.jumpMotor.canID", 11));
@@ -33,9 +41,15 @@ public class Frogger extends Subsystem {
 
         jumpMotor.setNeutralMode(NeutralMode.Brake);
 
+        DigitalInput encoderInputA = new DigitalInput(config.getInt("frogger.encoder.dio.A", 0));
+        DigitalInput encoderInputB = new DigitalInput(config.getInt("frogger.encoder.dio.B", 1));
+        encoder = new Encoder(encoderInputA, encoderInputB, false, EncodingType.k4X);
+
         froggerBottomWidget = Robot.programmerTab.add("froggerBottom", false).withPosition(13, 7).getEntry();
-       
-    }
+        //froggerTopWidget = Robot.programmerTab.add("froggerTop", false).withPosition(10, 7).getEntry();
+        resetEncoder();
+    } 
+
 
     @Override
     public void initDefaultCommand() {
@@ -44,9 +58,10 @@ public class Frogger extends Subsystem {
     @Override
     public void periodic() {
 
-        froggerBottomWidget.setBoolean(jumpMotor.getSensorCollection().isFwdLimitSwitchClosed());
-       
+        SmartDashboard.putNumber("Frogger Distance", encoderDistance());
 
+        froggerBottomWidget.setBoolean(jumpMotor.getSensorCollection().isFwdLimitSwitchClosed());
+    
      //   if (jumpMotor.get() == 0.0){
        //     IdleMode idleMode = braking ? IdleMode.kBrake : IdleMode.kCoast;
        // }
@@ -62,6 +77,15 @@ public class Frogger extends Subsystem {
 
     public void footStop() {
         jumpMotor.set(0.0);
+    }
+
+    public void resetEncoder() {
+        startingDistance = encoder.getDistance();
+    }
+
+    public double encoderDistance(){  
+        double currentDistance = encoder.getDistance();
+        return Math.abs(currentDistance - startingDistance) / TICKS_PER_INCH;
     }
 
     public void driveForward() {
